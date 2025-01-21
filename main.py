c5b9970a9e9dea6c040cacbb566cca6a2ae1bd94
maldaalda225 <malik@soje.cz>
2025-01-21 12:13:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,30 @@ def pocet():
     label_pocet_vet["text"] = f"Pocet vet: {pocet_vet}"
     label_pocet_slov["text"] = f"Pocet slov: {pocet_slov}"
     label_pocet_pismen["text"] = f"Pocet symbolu: {pocet_pismen}"
+
+root = tk.Tk()
+
+# Vstupní pole pro text
+tk.Label(root, text="Zadejte větu: ").grid(row=0, column=0)
+text_entry = tk.Entry(root)
+text_entry.grid(row=0, column=1)
+
+# Tlačítko pro výpočet
+vysledek_button = tk.Button(root, text="Vypocitej", command=pocet)
+vysledek_button.grid(row=1, column=1)
+
+# Label pro zobrazení výsledků
+label_zadana_veta = tk.Label(root, text="Zadana veta: ")
+label_zadana_veta.grid(row=2, column=0)
+
+label_pocet_vet = tk.Label(root, text="Pocet vet: ")
+label_pocet_vet.grid(row=3, column=0)
+
+label_pocet_slov = tk.Label(root, text="Pocet slov: ")
+label_pocet_slov.grid(row=4, column=0)
+
+label_pocet_pismen = tk.Label(root, text="Pocet symbolu: ")
+label_pocet_pismen.grid(row=5, column=0)
+
+# Spuštění hlavní smyčky aplikace
+root.mainloop()
